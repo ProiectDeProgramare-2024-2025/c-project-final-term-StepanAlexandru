a38611d7d2334ec4042d35b9ea92f0e0e25cb930
StepanAlexandru <alexandru.stepan05@e-uvt.ro>
2025-04-28 19:37:41 +0300

    Update main.c

--- main.c
+++ main.c
@@ -45,14 +45,14 @@ void clearScreen() {
 
 void printHeader() {
     printf(CYAN "\n=== SISTEM DE REZERVARE FARMACIE ===\n" RESET);
-    printf(GREEN "1 - Afiseaza produse disponibile\n" RESET);
-    printf(BLUE "2 - Rezerva produs\n" RESET);
-    printf(RED "3 - Anuleaza rezervare\n" RESET);
-    printf(YELLOW "4 - Cauta produs\n" RESET);
-    printf(MAGENTA "5 - Afiseaza rezervari\n" RESET);
-    printf(GREEN "6 - Adauga produs nou\n" RESET);
-    printf(RED "0 - Iesire\n" RESET);
-    printf(CYAN "=====================================\n" RESET);
+    printf(CYAN "1" RESET " - Afiseaza produse disponibile\n");
+    printf(CYAN "2" RESET " - Rezerva produs\n");
+    printf(CYAN "3" RESET " - Anuleaza rezervare\n");
+    printf(CYAN "4" RESET " - Cauta produs\n");
+    printf(CYAN "5" RESET " - Afiseaza rezervari\n");
+    printf(CYAN "6" RESET " - Adauga produs nou\n");
+    printf(CYAN "0" RESET " - Iesire\n");
+    printf(CYAN "\n================================================================\n" RESET);
 }
 
 int validateDate(const char* date) {
@@ -151,11 +151,11 @@ void saveReservations() {
 void showProducts() {
     clearScreen();
     printf(CYAN "\n=== PRODUSE DISPONIBILE ===\n" RESET);
-    printf(YELLOW "ID   Nume                 Pret    Stoc   Expira\n" RESET);
+    printf(YELLOW "ID   Nume                 Pret     Stoc   Expira\n" RESET);
     printf("------------------------------------------------\n");
 
     for (int i = 0; i < product_count; i++) {
-        printf("%-4d %-20s %-7.2f %-6d %s\n",
+        printf("%-4d " MAGENTA "%-20s" RESET " " GREEN "%-5.2f RON" RESET " %-6d " RED "%s" RESET "\n",
                products[i].id,
                products[i].name,
                products[i].price,
@@ -169,38 +169,38 @@ void addProduct() {
     printf(CYAN "\n=== ADAUGA PRODUS NOU ===\n" RESET);
 
     if (product_count >= MAX_PRODUSE) {
-        printf(RED "Nu mai pot fi adaugate produse!\n" RESET);
+        printf(RED "Nu mai pot fi adaugate produse! Limita maxima (%d) a fost atinsa.\n" RESET, MAX_PRODUSE);
         return;
     }
 
     Product p;
 
-    printf("ID produs: ");
+    printf("ID produs (numar intreg unic): ");
     while (scanf("%d", &p.id) != 1) {
-        printf(RED "Input invalid! Introdu un numar: " RESET);
+        printf(RED "Input invalid! Introdu un numar intreg pentru ID: " RESET);
         while (getchar() != '\n');
     }
 
-    printf("Nume produs: ");
+    printf("Nume produs (max %d caractere fara spatii): ", MAX_NAME-1);
     scanf("%49s", p.name);
 
-    printf("Pret: ");
+    printf("Pret (format 00.00, valoare pozitiva): ");
     while (scanf("%f", &p.price) != 1 || p.price <= 0) {
-        printf(RED "Pret invalid! Introdu un numar pozitiv: " RESET);
+        printf(RED "Pret invalid! Introdu un numar pozitiv (ex: 12.99): " RESET);
         while (getchar() != '\n');
     }
 
-    printf("Stoc initial: ");
+    printf("Stoc initial (numar intreg pozitiv): ");
     while (scanf("%d", &p.stock) != 1 || p.stock < 0) {
-        printf(RED "Stoc invalid! Introdu un numar pozitiv: " RESET);
+        printf(RED "Stoc invalid! Introdu un numar intreg pozitiv: " RESET);
         while (getchar() != '\n');
     }
 
     do {
-        printf("Data expirarii (ZZ/LL/AAAA): ");
+        printf("Data expirarii (format ZZ/LL/AAAA, ex: 15/06/2024): ");
         scanf("%10s", p.expiry_date);
         if (!validateDate(p.expiry_date)) {
-            printf(RED "Format invalid! Foloseste ZZ/LL/AAAA\n" RESET);
+            printf(RED "Format invalid! Foloseste exact formatul ZZ/LL/AAAA\n" RESET);
         }
     } while (!validateDate(p.expiry_date));
 
@@ -218,32 +218,32 @@ void reserveProduct() {
     Reservation r;
     int product_id, found = 0;
 
-    printf("\nIntroduceti ID-ul produsului: ");
+    printf("\nIntroduceti ID-ul produsului pe care doriti sa il rezervati: ");
     while (scanf("%d", &product_id) != 1) {
-        printf(RED "Input invalid! Introdu un numar: " RESET);
+        printf(RED "Input invalid! Introdu un numar intreg pentru ID: " RESET);
         while (getchar() != '\n');
     }
 
     for (int i = 0; i < product_count; i++) {
         if (products[i].id == product_id) {
             found = 1;
-            printf("Produs selectat: " GREEN "%s" RESET " (Stoc: %d)\n",
+            printf("Produs selectat: " MAGENTA "%s" RESET " (Stoc disponibil: " GREEN "%d" RESET ")\n",
                    products[i].name, products[i].stock);
 
-            printf("Introduceti numele dvs.: ");
+            printf("Introduceti numele dvs. (max %d caractere): ", MAX_NAME-1);
             scanf("%49s", r.client_name);
 
             do {
-                printf("Cantitate: ");
+                printf("Cantitatea dorita (numar intreg pozitiv): ");
                 while (scanf("%d", &r.quantity) != 1) {
-                    printf(RED "Input invalid! Introdu un numar: " RESET);
+                    printf(RED "Input invalid! Introdu un numar intreg: " RESET);
                     while (getchar() != '\n');
                 }
 
                 if (r.quantity <= 0) {
                     printf(RED "Cantitatea trebuie sa fie pozitiva!\n" RESET);
                 } else if (r.quantity > products[i].stock) {
-                    printf(RED "Stoc insuficient! Disponibil: %d\n" RESET, products[i].stock);
+                    printf(RED "Stoc insuficient! Disponibil: " GREEN "%d" RESET "\n", products[i].stock);
                 }
             } while (r.quantity <= 0 || r.quantity > products[i].stock);
 
@@ -251,20 +251,28 @@ void reserveProduct() {
 
             r.id = reservation_count + 1;
             r.product_id = product_id;
-            strcpy(r.reservation_date, "01/01/2023");
+
+            do {
+                printf("Data rezervarii (format ZZ/LL/AAAA): ");
+                scanf("%10s", r.reservation_date);
+                if (!validateDate(r.reservation_date)) {
+                    printf(RED "Format invalid! Foloseste exact formatul ZZ/LL/AAAA\n" RESET);
+                }
+            } while (!validateDate(r.reservation_date));
 
             reservations[reservation_count++] = r;
             saveProducts();
             saveReservations();
 
             printf(GREEN "\nRezervare efectuata cu succes!\n" RESET);
-            printf("Numar rezervare: " BLUE "%d\n" RESET, r.id);
+            printf("Numar rezervare: " BLUE "%d" RESET "\n", r.id);
+            printf("Data rezervarii: " YELLOW "%s" RESET "\n", r.reservation_date);
             break;
         }
     }
 
     if (!found) {
-        printf(RED "\nProdusul nu a fost gasit!\n" RESET);
+        printf(RED "\nProdusul cu ID-ul %d nu a fost gasit!\n" RESET, product_id);
     }
 }
 
@@ -281,7 +289,7 @@ void showReservations() {
     printf("------------------------------------------------\n");
 
     for (int i = 0; i < reservation_count; i++) {
-        printf("%-4d %-10d %-15s %-8d %s\n",
+        printf("%-4d %-10d " MAGENTA "%-15s" RESET " %-8d " BLUE "%s" RESET "\n",
                reservations[i].id,
                reservations[i].product_id,
                reservations[i].client_name,
@@ -342,13 +350,13 @@ void searchProduct() {
     scanf("%49s", search_term);
 
     printf(YELLOW "\nRezultatele cautarii:\n" RESET);
-    printf("ID   Nume                 Pret    Stoc   Expira\n");
+    printf("ID   Nume                 Pret     Stoc   Expira\n");
     printf("------------------------------------------------\n");
 
     int found = 0;
     for (int i = 0; i < product_count; i++) {
         if (strstr(products[i].name, search_term) != NULL) {
-            printf("%-4d %-20s %-7.2f %-6d %s\n",
+            printf("%-4d " MAGENTA "%-20s" RESET " " GREEN "%-6.2f RON" RESET " %-6d " RED "%s" RESET "\n",
                    products[i].id,
                    products[i].name,
                    products[i].price,
